3a70f7ef01e90e2e1d969af68ae29446dad5f7b3
hyeonhoS <hyunho2005@naver.com>
2025-07-07 19:23:53 +0900

    #3.4 Parallel Requests

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,21 @@
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideo(id: string) {
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
 export default async function MovieDetail({
-  params,
+  params: { id },
 }: {
   params: { id: string };
 }) {
-  const { id } = await params;
-
-  return <h1>Movie Detail {id}</h1>;
+  const movie = await getMovie(id);
+  const video = await getVideo(id);
+  return <h1>{movie.title}</h1>;
 }
